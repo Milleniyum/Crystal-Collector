1d619553dda093d3053a57b1fe1669df7c6514cc
Jamie DeLong <jdlong1980@gmail.com>
2018-12-10 20:02:43 -0500

    updated all files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,36 @@
-var wins = 0, losses = 0, score = 0;
+var wins = 0, losses = 0, score, randNum;
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 function newGame() {
-    $("#score").text(score);
-    $("#random-num").text(getRandomNumber(19, 120));
+    score = 0;
+    randNum = getRandomNumber(19, 120);
+    $("#random-num").text(randNum);
     $("#gem-1").val(getRandomNumber(1, 12));
     $("#gem-2").val(getRandomNumber(1, 12));
     $("#gem-3").val(getRandomNumber(1, 12));
     $("#gem-4").val(getRandomNumber(1, 12));
+    $("#score").text(score);
 };
+
+$(".gem").on("click", function () {
+    score += parseInt($(this).val());
+    $("#score").text(score);
+    $("#result").text("");
+
+    if (score === randNum) {
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        $("#result").text("You Won!");
+        newGame();
+    } else if (score > randNum) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        $("#result").text("You Lost!");
+        newGame();
+    };
+});
 
 newGame();
